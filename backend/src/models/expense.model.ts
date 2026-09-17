6bcf362a1e2e1888f84a54805b9024b58fc1b5f4
Kanaga Subramanian <kanaga@genome.com>
2026-09-17 13:25:28 +0530

    Feat: Expense model added

---
+++ backend/src/models/expense.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+const expenseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: [true, "Payment Method is required"],
+      trim: true,
+    },
+    shopName: {
+      type: String,
+      trim: true,
+    },
+    date: {
+  type: Date,
+  required: true
+}
+    notes: {
+      type: String,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount is required"],
+      min: [1, "Amount must be greater than 0"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Expense = mongoose.model("Expense", expenseSchema);
