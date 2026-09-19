6d6d1012256073a65bcebaa576f3923bb32ff9c9
Kanaga Subramanian <kanaga@genome.com>
2026-09-19 11:28:46 +0530

    Feat: db connection ordered and added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import app from "./app.js";
 import { connectDatabase } from "./config/database.js";
 const port = process.env.PORT??3000
-connectDatabase()
-app.listen(port, () => {
-  console.log(`Server will run on port ${port}`);
-});
+const startServer = async () => {
+  await connectDatabase();
+
+  app.listen(port, () => {
+    console.log(`Server will run on port ${port}`);
+  });
+};
+
+startServer();
